98997a4c9cfc2c683c70a123fb50046be169ef7d
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-19 10:13:16 +0300

    split App into examples and core concepts components

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,9 +1,25 @@
-export default function CoreConcepts({ image, title, description }) {
+import React from "react";
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "./CoreConcept";
+function CoreConcepts() {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {/* <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} /> */}
+        {CORE_CONCEPTS.map((conceptItem) => {
+          return <CoreConcept {...conceptItem} />;
+        })}
+      </ul>
+    </section>
   );
 }
+
+export default CoreConcepts;
